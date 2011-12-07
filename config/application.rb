fc6b15f2e407e1246982453500f6bfed2cccfa33
Yoichi Inazaki <y.inazaki@gmail.com>
2011-12-07 18:38:49 +0900

    Added a Pages controller

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,15 @@ require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+#require "coffee"
+#require "jquery-rails"
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
+
 end
 
 module SampleApp
